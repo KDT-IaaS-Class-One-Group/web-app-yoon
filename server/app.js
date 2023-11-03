9dbe3f5a354bec612a1e93cf8a2c2b547d1fadc3
YoonJunHyeon <qmdldos@gmail.com>
2023-11-03 16:06:33 +0900

    객체 타입이 아니라 문자열로 들어간다. 이제 객체 생성하면 되겠다.

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@ const app = express();
 
 // 테스트 전용 옵션
 const path = require('path')
-const bodyParser = require('body-parser');
-const fs = require('fs').promises;
+// const bodyParser = require('body-parser');
+const fs = require('fs');
 
+
+// app.use(bodyParser.json())
+app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -25,24 +28,45 @@ app.get('/data', (req, res) => {
   res.send(data);
 });
 
-
 // * writeData 전용 라우팅 /save-text
 // * talkData.json을 읽어온 후, 
-app.post('/save-text', async (req, res) => {
-  const url = req.body.url;
+// 1. 다시 문서화 
+// 2. 새로운 데이터 업로드 후에
+// 3. 업데이트 된 내용을 다시 저장한다.
+app.use(express.json());
+app.post('/save-text', (req, res) => {
+    const text = req.body.userInput;
+    console.table(req.body); // 확인 완료
+    // 파일 경로 설정
+    const filePath = path.join(__dirname, '../data/talkData.json');
+    console.log(filePath);
+    // 파일의 현재 내용 읽기
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to read the file.' });
+        }
+        console.log(data);
+        // 1. 다시 문서화
+        let texts = [];
+        if (data) {
+            texts = JSON.parse(data);
+        }
+        console.log(texts);
 
-  // 외부 placeholder와 같은 주소를 가져옵니다. (이 예제에서는 가져오는 코드는 생략되었습니다.)
+        // 2. 새로운 text 추가
+        texts.push(text);
 
-  // asset 폴더에 정보 저장
-  const data = {
-      lastSubmittedURL: url,
-      timestamp: new Date().toISOString()
-  };
+        // 3.. 파일에 업데이트 된 내용 저장
+        fs.writeFile(filePath, JSON.stringify(texts, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Failed to write to the file.' });
+            }
+          });
+        });
+        res.redirect('/');  // 사용자를 결과 페이지로 리디렉션합니다.
+        console.log('redirect')
+      });
 
-  await fs.writeFile('./data/talkData.json', JSON.stringify(data, null, 2));
-
-  res.redirect('/');  // 사용자를 결과 페이지로 리디렉션합니다.
-});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
