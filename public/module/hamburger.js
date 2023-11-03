e47bb7a1995982d6bcd5bf02b2130f0559207902
YoonJunHyeon <qmdldos@gmail.com>
2023-11-03 20:04:59 +0900

    오우쮓 날려버린 2시간쓰

--- public/module/hamburger.js
+++ public/module/hamburger.js
@@ -6,26 +6,33 @@ const root = document.getElementById('root');
 const container = document.getElementById('container');
 console.dir(root);
 
-const widthSetInterval = (range) => {
-  let temp = 0;
-  let nowWidth = 80;
-  let movement = setInterval(()=>{
-    if(temp < range) {
-      temp++;
-      console.log(temp);
-      container.style.width = nowWidth + temp+"vw";
-    } else {
-      clearInterval(movement)
+// const widthSetInterval = (range) => {
+//   let temp = 0;
+//   let nowWidth = 80;
+//   let movement = setInterval(()=>{
+//     if(temp < range) {
+//       temp++;
+//       console.log(temp);
+//       container.style.width = nowWidth + temp+"vw";
+//     } else {
+  //       clearInterval(movement)
+  //     }
+  //   }, 1)
+  // }
+  
+  // widthSetInterval(20);
+  
+  
+  // 1. 버튼을 누르면 이벤트
+  topbtn.addEventListener('click',()=>{
+    // 햄버거 컨테이너를 왼쪽으로 이동
+    if(container.style.width=== "100vw"){
+      container.style.width= "80vw";
+      hbgCont.style.left = "0";
+    }else{
+      container.style.width = "100vw";
+      hbgCont.style.left = "-15vw";
     }
-  }, 1)
-}
-
-widthSetInterval(20);
-
-
-// 1. 버튼을 누르면 이벤트
-topbtn.addEventListener('click',()=>{
-  // 햄버거 컨테이너를 왼쪽으로 이동
 
 
   // root의 스타일을 추가 제거
