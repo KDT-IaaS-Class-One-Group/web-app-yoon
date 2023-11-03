1892f4f42e45e666d5a99d348176fe68fd0f94de
YoonJunHyeon <qmdldos@gmail.com>
2023-11-03 20:43:07 +0900

    클래스 모듈 생성

---
+++ public/module/class.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(userInput){
+    this.type = "user";
+    this.message = userInput
+    this.timestamp = `${new Date().getHours()}시 ${new Date().getMinutes()}분`
+  }
+}
+
+const exam = new User('안녕하십니까');
+console.log(exam);
