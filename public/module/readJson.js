ac5638e4ea6aca61a53c11b89e231eee6bc7c8db
YoonJunHyeon <qmdldos@gmail.com>
2023-11-06 15:58:47 +0900

    readJson을 fetch와 then을 혼용하여 사용하기 보단 일관성 잇도록 작성해본다. try/catch

--- public/module/readJson.js
+++ public/module/readJson.js
@@ -1,16 +1,15 @@
-
 /**
- * 비동기로 "/read-data로 " 요청을 보냅니다.
- * @param {function} callback 비동기적으로 data를 다룰 함수를뒤에 작성합니다.
- * @return {Array} 리턴은 data 배열이 넘어갑니다.
+ * 비동기로 "/read-data"로 요청을 보냅니다.
+ * @param {function} callback 비동기적으로 data를 다룰 함수를 뒤에 작성합니다.
  */
-export const readJson = async ( callback )=>{
+export const readJson = async (callback) => {
   console.log(`readJson 시작`);
-  const response = await fetch('/read-data') // 요청 날리기
-  response.json() // 받은 데이터 변환
-  .then(data => {
+  try {
+    const response = await fetch('/read-data'); // 요청 날리기
+    const data = await response.json(); // 받은 데이터 변환
     callback(data);
     console.log('readJson이 작동했다.');
-  })
-  .catch(err => console.log("에러발생",err))
+  } catch (err) {
+    console.log("에러발생", err);
+  }
 };
